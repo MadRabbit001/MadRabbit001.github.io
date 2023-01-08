3a51f924214a318e41808e6af9e99598935786f1
MadRabbit001 <101290160+MadRabbit001@users.noreply.github.com>
2023-01-08 19:00:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,46 @@
 // Array of user objects
 var users = [
-    {
-        email: "liron.ben-shushan@priority-software.com",
-        password: "password1"
-    },
-    {
-        email: "omri.danino@priority-software.com",
-        password: "password2"
-    },
-    {
-        email: "shir.ben.gigi@priority-software.com",
-        password: "password3"
-    }
+  {
+    email: "liron.ben-shushan@priority-software.com",
+    password: "password1"
+  },
+  {
+    email: "jane.doe@example.com",
+    password: "password2"
+  },
+  {
+    email: "john.smith@example.com",
+    password: "password3"
+  }
 ];
 
 // Function to validate the email and password entered by the user
-function validateForm(event) {
-    console.log("validateForm() called");  // Debugging line
+function validateForm() {
+  // Get the email and password values from the form
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
 
-    // Get the email and password values from the form
-    var email = document.getElementById("email").value.toLowerCase();
-    var password = document.getElementById("password").value;
-
-    // Check if the email and password match a user in the array
-    if (validateCredentials(email, password)) {
-        // Email and password match a user in the array
-        // Allow the form submission
-        return true;
-    } else {
-        // Email and password do not match any users in the array
-        // Prevent the form submission and display an error message
-        event.preventDefault();
-        alert("Incorrect email or password");
-        return false;
-    }
+  // Check if the email and password match a user in the array
+  if (validateCredentials(email, password)) {
+    // Email and password match a user in the array
+    // Allow the form submission
+    return true;
+  } else {
+    // Email and password do not match any users in the array
+    // Prevent the form submission and display an error message
+    alert("Incorrect email or password");
+    return false;
+  }
 }
-
-
 
 // Function to check if a given email and password match a user in the array
 function validateCredentials(email, password) {
-    for (var i = 0; i < users.length; i++) {
-        if (users[i].email == email && users[i].password == password) {
-            // Email and password match a user in the array
-            return true;
-        }
+  for (var i = 0; i < users.length; i++) {
+    if (users[i].email == email && users[i].password == password) {
+      // Email and password match a user in the array
+      return true;
     }
-    // Email and password do not match any users in the array
-    return false;
+  }
+  // Email and password do not match any users in the array
+  return false;
 }
