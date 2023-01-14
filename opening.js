2558f37151b5679b8cd31a3e728ab7b43317971d
MadRabbit001 <101290160+MadRabbit001@users.noreply.github.com>
2023-01-15 00:25:08 +0200

    Add files via upload

---
+++ opening.js
@@ -0,0 +1,15 @@
+// let button = document.getElementById("show");
+// let div = document.getElementById("show-me");
+
+// button.addEventListener("click", function () {
+//   if (div.style.display === "none") {
+//     div.style.display = "block";
+//   } else {
+//     div.style.display = "none";
+//   }
+// });
+let mad = document.querySelector(".hey");
+
+function rabbit() {}
+
+mad.addEventListener("click", function () {});
