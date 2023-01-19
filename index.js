da290800bda358d45d6696e10e61db05eda4b653
MadRabbit001 <101290160+MadRabbit001@users.noreply.github.com>
2023-01-19 16:23:09 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 var users = [
   {
     email: "liron.ben-shushan@priority-software.com",
-    password: "password1"
+    password: "s10EsSHITd32HHvvd465?4d1",
   },
   {
-    email: "jane.doe@example.com",
-    password: "password2"
+    email: "omri.danino@priority-software.com",
+    password: "s10EsSHITd32HHvvd465?4d2",
   },
   {
-    email: "john.smith@example.com",
-    password: "password3"
-  }
+    email: "shir.ben.gigi@priority-software.com",
+    password: "s10EsSHITd32HHvvd465?4d3",
+  },
 ];
 
 // Function to validate the email and password entered by the user
@@ -19,6 +19,17 @@ function validateForm() {
   // Get the email and password values from the form
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
+
+  email = email.toLowerCase();
+
+  password =
+    password.substring(3, 4) +
+    "10E" +
+    password.substring(3, 4) +
+    "SHIT" +
+    password.substring(7, 8) +
+    "32HHvvd465?4" +
+    password.substring(7);
 
   // Check if the email and password match a user in the array
   if (validateCredentials(email, password)) {
@@ -44,3 +55,11 @@ function validateCredentials(email, password) {
   // Email and password do not match any users in the array
   return false;
 }
+
+window.onload = function () {
+  document.getElementById("loading-animation").style.display = "none";
+};
+
+window.onbeforeunload = function () {
+  document.getElementById("loading-animation").style.display = "block";
+};
--- opening.js
+++ opening.js
@@ -9,7 +9,135 @@
 //   }
 // });
 let mad = document.querySelector(".hey");
+let game = document.querySelector(".game");
 
-function rabbit() {}
+// let bg = document.querySelector(".footer");
+let open = document.querySelector(".open");
+let menu1 = document.querySelector(".one");
+let menu2 = document.querySelector(".two");
+let menu3 = document.querySelector(".three");
+let menu4 = document.querySelector(".four");
+let menu5 = document.querySelector(".five");
+let menu6 = document.querySelector(".six");
+let menu7 = document.querySelector(".seven");
 
-mad.addEventListener("click", function () {});
+let menu2A = document.querySelector(".twoA");
+let menu2B = document.querySelector(".twoB");
+let menu2B2 = document.querySelector(".twoB2");
+
+function rabbit() {
+  game.classList.toggle("hidden");
+  setTimeout(niceshadow, 200);
+  setTimeout(badshadow, 400);
+}
+
+function niceshadow() {
+  mad.setAttribute("style", "box-shadow: inset 2px 2px 5px #333");
+}
+
+function badshadow() {
+  mad.setAttribute(
+    "style",
+    "box-shadow: 2px 2px 5px #333, -2px -2px 5px #333;"
+  );
+}
+
+//close all window
+function closebg() {
+  menu1.classList.add("hidden");
+  menu2.classList.add("hidden");
+  menu2A.classList.add("hidden");
+  menu2B.classList.add("hidden");
+  menu2B2.classList.add("hidden");
+}
+//first line of window : 7
+function openWin1() {
+  menu1.classList.toggle("hidden");
+  menu2.classList.add("hidden");
+  menu3.classList.add("hidden");
+  menu4.classList.add("hidden");
+  menu5.classList.add("hidden");
+  menu6.classList.add("hidden");
+  menu7.classList.add("hidden");
+}
+
+function openWin2() {
+  menu2.classList.toggle("hidden");
+  menu1.classList.add("hidden");
+  menu3.classList.add("hidden");
+  menu4.classList.add("hidden");
+  menu5.classList.add("hidden");
+  menu6.classList.add("hidden");
+  menu7.classList.add("hidden");
+}
+
+function openWin3() {
+  menu3.classList.toggle("hidden");
+  menu1.classList.add("hidden");
+  menu2.classList.add("hidden");
+  menu4.classList.add("hidden");
+  menu5.classList.add("hidden");
+  menu6.classList.add("hidden");
+  menu7.classList.add("hidden");
+}
+
+function openWin4() {
+  menu4.classList.toggle("hidden");
+  menu2.classList.add("hidden");
+  menu3.classList.add("hidden");
+  menu1.classList.add("hidden");
+  menu5.classList.add("hidden");
+  menu6.classList.add("hidden");
+  menu7.classList.add("hidden");
+}
+
+function openWin5() {
+  menu5.classList.toggle("hidden");
+  menu2.classList.add("hidden");
+  menu3.classList.add("hidden");
+  menu4.classList.add("hidden");
+  menu1.classList.add("hidden");
+  menu6.classList.add("hidden");
+  menu7.classList.add("hidden");
+}
+
+function openWin6() {
+  menu6.classList.toggle("hidden");
+  menu2.classList.add("hidden");
+  menu3.classList.add("hidden");
+  menu4.classList.add("hidden");
+  menu5.classList.add("hidden");
+  menu1.classList.add("hidden");
+  menu7.classList.add("hidden");
+}
+
+function openWin7() {
+  menu7.classList.toggle("hidden");
+  menu2.classList.add("hidden");
+  menu3.classList.add("hidden");
+  menu4.classList.add("hidden");
+  menu5.classList.add("hidden");
+  menu6.classList.add("hidden");
+  menu1.classList.add("hidden");
+}
+
+// second line of window : 3
+function openSecWin2() {
+  menu2A.classList.toggle("hidden");
+  menu2.classList.toggle("hidden");
+}
+
+// function third line of window : 3
+function openThrdWin2() {
+  menu2B.classList.toggle("hidden");
+  menu2A.classList.toggle("hidden");
+
+  if (!menu2B2.classList.contains("hidden")) menu2B2.classList.add("hidden");
+}
+
+function openThrdWin2B() {
+  menu2B2.classList.toggle("hidden");
+  menu2A.classList.toggle("hidden");
+
+  if (!menu2B.classList.contains("hidden")) menu2B.classList.add("hidden");
+}
