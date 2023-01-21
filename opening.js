8425918f1fd2c4555d9a9b2824a394dfcf4278e7
MadRabbit001 <101290160+MadRabbit001@users.noreply.github.com>
2023-01-21 14:10:57 +0200

    Add files via upload

--- opening.js
+++ opening.js
@@ -170,6 +170,91 @@ select.addEventListener("change", function () {
     window.location.replace("priority.html#iis");
   } else if (this.value === "option2") {
     window.location.replace("priority.html#reciept");
+  } else if (this.value === "option3") {
+    window.location.replace("priority.html#image");
+  } else if (this.value === "option4") {
+    window.location.replace("priority.html#connect");
+  } else if (this.value === "option5") {
+    window.location.replace("priority.html#ip");
+  } else if (this.value === "option6") {
+    window.location.replace("priority.html#sync");
+  } else if (this.value === "option7") {
+    window.location.replace("priority.html#gift");
+  }
+  //    else if (this.value === "option8") {
+  //   window.location.replace("priority.html#");
+  // } else if (this.value === "option9") {
+  //   window.location.replace("priority.html#reciept");}
+  else if (this.value === "option10") {
+    window.location.replace("priority.html#return");
+  } else if (this.value === "option11") {
+    window.location.replace("priority.html#dis");
+  } else if (this.value === "option12") {
+    window.location.replace("priority.html#hand");
+  } else if (this.value === "option13") {
+    window.location.replace("priority.html#worker");
+  } else if (this.value === "option14") {
+    window.location.replace("priority.html#print");
+  }
+  // else if (this.value === "option15") {
+  //   window.location.replace("priority.html#");
+  // }
+  else if (this.value === "option16") {
+    window.location.replace("priority.html#mg");
+  } else if (this.value === "option17") {
+    window.location.replace("priority.html#browser");
+  } else if (this.value === "option18") {
+    window.location.replace("priority.html#login");
+  } else if (this.value === "option19") {
+    window.location.replace("priority.html#setuser");
+  } else if (this.value === "option20") {
+    window.location.replace("priority.html#cer");
+  } else if (this.value === "option21") {
+    window.location.replace("priority.html#info");
+  } else if (this.value === "option22") {
+    window.location.replace("priority.html#order");
+  } else if (this.value === "option23") {
+    window.location.replace("priority.html#extension");
+  } else if (this.value === "option24") {
+    window.location.replace("priority.html#ern");
+  } else if (this.value === "option25") {
+    window.location.replace("priority.html#mput");
+  } else if (this.value === "option26") {
+    window.location.replace("priority.html#cash");
+  } else if (this.value === "option27") {
+    window.location.replace("priority.html#view");
+  } else if (this.value === "option28") {
+    window.location.replace("priority.html#nom");
+  } else if (this.value === "option29") {
+    window.location.replace("priority.html#allow");
+  } else if (this.value === "option30") {
+    window.location.replace("priority.html#pass");
+  } else if (this.value === "option31") {
+    window.location.replace("priority.html#sale");
+  } else if (this.value === "option32") {
+    window.location.replace("priority.html#down");
+  } else if (this.value === "option33") {
+    window.location.replace("priority.html#file");
+  } else if (this.value === "option34") {
+    window.location.replace("priority.html#sent");
+  } else if (this.value === "option35") {
+    window.location.replace("priority.html#log");
+  } else if (this.value === "option36") {
+    window.location.replace("priority.html#dump");
+  } else if (this.value === "option37") {
+    window.location.replace("priority.html#conf");
+  } else if (this.value === "option38") {
+    window.location.replace("priority.html#up");
+  } else if (this.value === "option39") {
+    window.location.replace("priority.html#back");
+  } else if (this.value === "option40") {
+    window.location.replace("priority.html#no");
+  } else if (this.value === "option41") {
+    window.location.replace("priority.html#stat");
+  } else if (this.value === "option42") {
+    window.location.replace("priority.html#branch");
+  } else if (this.value === "option43") {
+    window.location.replace("priority.html#cancel");
   }
 });
 
